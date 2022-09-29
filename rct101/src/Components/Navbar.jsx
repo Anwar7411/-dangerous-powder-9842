445824601ac17cc8fbe1634f5d8041d7b7df7b5b
Anwar Basha <bashaa7411@gmail.com>
2022-09-29 21:37:17 +0530

    added day2 branch

--- rct101/src/Components/Navbar.jsx
+++ rct101/src/Components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import styles from "./Navbar.module.css"
 import { Search2Icon} from '@chakra-ui/icons'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate} from 'react-router-dom'
 const Navbar = () => {
-  
+  const navigate=useNavigate();
   return (
     <div className={styles.navbar}>
         <div >
-           <Link to="/"> <img src="https://english.cdn.zeenews.com/static/apprun/dna/icons/logodna.png" alt="logo" /></Link>
+            <img src="https://english.cdn.zeenews.com/static/apprun/dna/icons/logodna.png" alt="logo" onClick={()=>navigate("/")}/>
         </div>
         <div>
-        <Search2Icon w={20} h={40} />
+        <Search2Icon w={10} h={6} />
         </div>
     </div>
   )
--- rct101/src/Pages/Home.jsx
+++ rct101/src/Pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import News from '../Components/News'
+import Menu from '../Components/Menu'
+import { Flex, Container, Grid } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container maxW={"1400px"}  mt="15px">
+      <Grid templateColumns={"repeat(3,1fr)"}gap={5}>
+        <Box >
+          <Menu />
+        </Box>
+        <Box w="700px">
+          <News />
+        </Box>
+        <Box w="300px">
+          <News />
+        </Box>
+      </Grid>
+    </Container>
   )
 }
 
